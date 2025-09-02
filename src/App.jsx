915f90a0978d0053cff92c1a8ae4901a03c4ef16
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2025-09-02 13:06:21 +0900

    add.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Skills from './pages/Skill';
-import Projects from './pages/Projects';
-import Activity from './pages/Activity';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Skills from "./pages/Skill";
+import Projects from "./pages/Projects";
+import Activity from "./pages/Activity";
 
 function App() {
   return (
     <div className="min-w-[400px] bg-white dark:bg-slate-900">
-        <Home />
-        <Header />
-        <About/>
-        <Skills/>
-        <Projects/>
-        <Activity/>
+      <Home />
+      <Header />
+      <About />
+      <Skills />
+      <Projects />
+      <Activity />
       <Footer />
     </div>
   );
